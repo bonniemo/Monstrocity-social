894a419be1bad476d58e835fe1572a29678c85e8
bonniemo <moller.bon@gmail.com>
2024-11-25 13:54:19 +0100

    avatars working for posts

--- utils/getAvatarSource.ts
+++ utils/getAvatarSource.ts
@@ -1,6 +1,7 @@
 import avatarMapping from "./avatarMapping";
+import { monstersMapping } from "./monstersMapping";
 
 export const getAvatarSource = (author: string) => {
-    const avatarKey = avatarMapping[author] || avatarMapping["default.png"];
-    return avatarKey;
-  };
+  const avatarKey = monstersMapping[author] || "default.png"; 
+  return avatarMapping[avatarKey] || avatarMapping["default.png"]; 
+};
---
+++ utils/monstersMapping.ts
@@ -0,0 +1,13 @@
+export const monstersMapping: Record<string, string> = {
+    "Nyx Umbra": "nyx-umbra.png",
+    "Ignatius Flameheart": "ignatius-flameheart.png",
+    "Luna Lumina": "luna-lumina.png",
+    "Terrus Mosshide": "terrus-mosshide.png",
+    "Zephyr Skywhisper": "zephyr-skywhisper.png",
+    "Eclipse Nocturne": "eclipse-nocturne.png",
+    "Pyra Emberglow": "pyra-emberglow.png",
+    "Selena Dewdrop": "selena-dewdrop.png",
+    "Orion Starcaster": "orion-starcaster.png",
+    "Aurora Glimmer": "aurora-glimmer.png",
+  };
+  
